a5fcd7b421b702e082c99d9f2820154f8c8b256d
ToxianoXinto <oluwatosinbalogun64@gmail.com>
2023-07-16 04:13:44 -0700

    L

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,4 +11,4 @@ int main(void)
 	write(2,
 	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
-}a C program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line.
+}
