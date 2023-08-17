b03de77c5fd5fadf27823e85e04d44e94fc7c813
ToxianoXinto <oluwatosinbalogun64@gmail.com>
2023-08-16 17:38:32 -0700

    L

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,4 +1,3 @@
-
 #include "function_pointers.h"
 /**
  * int_index - return index place if comparison = true, else -1
@@ -11,11 +10,10 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-
 	if (array == NULL || size <= 0 || cmp == NULL)
 		return (-1);
 
-		for (i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]))
 			return (i);
