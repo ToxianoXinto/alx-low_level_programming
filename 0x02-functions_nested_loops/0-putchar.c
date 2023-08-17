f41d95c0b1d9a36c5872bd552c17d35dbb62b8ae
ToxianoXinto <oluwatosinbalogun64@gmail.com>
2023-08-16 17:04:55 -0700

    update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,17 +4,16 @@
  * written by tosin 
  * Return: 0
  */
-
 int main(void)
 {
-_putchar ('_');
-_putchar ('p');
-_putchar ('u');
-_putchar ('t');
-_putchar ('c');
-_putchar ('h');
-_putchar ('a');
-_putchar ('r');
-_putchar ('\n');
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putcharu('\n');
 return(0);
 }
